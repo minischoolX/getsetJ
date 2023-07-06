76c47927ae1106a99765c6a38156e47a786293d3
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-07-06 21:17:42 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -23,9 +23,9 @@ import android.text.InputType;
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
-    private EditText searchEditText;
-    private ImageButton settingsButton;
-    private Boolean isAdBlocked;
+//    private EditText searchEditText;
+//    private ImageButton settingsButton;
+//    private Boolean isAdBlocked;
 
     @Override
     @SuppressLint("SetJavaScriptEnabled")
@@ -34,68 +34,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         mWebView = findViewById(R.id.activity_main_webview);
-        searchEditText = findViewById(R.id.searchEditText);
-        searchEditText.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
-        settingsButton = findViewById(R.id.settingsButton);
+//        searchEditText = findViewById(R.id.searchEditText);
+//        searchEditText.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
+//        settingsButton = findViewById(R.id.settingsButton);
 
-        isAdBlocked = getAdBlockSetting();
+//        isAdBlocked = getAdBlockSetting();
         
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new ModWebViewClient());
 
         // Set OnEditorActionListener for searchEditText
-        searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_GO || (event != null && event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
-                    performSearch();
-                    return true;
-                }
-                return false;
-            }
-        });
+  //      searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+  //          @Override
+  //          public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+  //              if (actionId == EditorInfo.IME_ACTION_GO || (event != null && event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) {
+  //                  performSearch();
+  //                  return true;
+  //              }
+  //              return false;
+  //          }
+  //      });
 
         // Set click listener for settingsButton
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openSettings();
-            }
-        });
+  //      settingsButton.setOnClickListener(new View.OnClickListener() {
+  //          @Override
+  //          public void onClick(View v) {
+  //              openSettings();
+  //          }
+  //      });
         
         mWebView.loadUrl("https://google.com");
     }
 
-    private boolean getAdBlockSetting() {
-        SharedPreferences adBlockSharedPreferences = getSharedPreferences("AdBlockPref", Context.MODE_PRIVATE);
-        return adBlockSharedPreferences.getBoolean("adBlock", false);
-    }
+   // private boolean getAdBlockSetting() {
+   //     SharedPreferences adBlockSharedPreferences = getSharedPreferences("AdBlockPref", Context.MODE_PRIVATE);
+   //     return adBlockSharedPreferences.getBoolean("adBlock", false);
+   // }
 
 
-    private void performSearch() {
-        String searchText = searchEditText.getText().toString().trim();
+   // private void performSearch() {
+   //     String searchText = searchEditText.getText().toString().trim();
 
-        if (isValidUrl(searchText)) {
-            // Load URL in the WebView
-            mWebView.loadUrl(searchText);
-        } else {
-            // Perform a Google search
-            String googleSearchUrl = "https://www.google.com/search?q=" + searchText;
-            mWebView.loadUrl(googleSearchUrl);
-        }
-    }
+   //     if (isValidUrl(searchText)) {
+   //         // Load URL in the WebView
+   //         mWebView.loadUrl(searchText);
+   //     } else {
+   //         // Perform a Google search
+   //         String googleSearchUrl = "https://www.google.com/search?q=" + searchText;
+   //         mWebView.loadUrl(googleSearchUrl);
+   //     }
+   // }
 
-    // Helper method to check if the entered text is a valid URL
-    private boolean isValidUrl(String url) {
-        return Patterns.WEB_URL.matcher(url).matches();
-    }
+   // // Helper method to check if the entered text is a valid URL
+   // private boolean isValidUrl(String url) {
+   //     return Patterns.WEB_URL.matcher(url).matches();
+   // }
 
-    private void openSettings() {
-        // Navigate to the settings screen
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
+   // private void openSettings() {
+   //     // Navigate to the settings screen
+   //     Intent intent = new Intent(this, SettingsActivity.class);
+   //     startActivity(intent);
+   // }
     
     @Override
     public void onBackPressed() {
